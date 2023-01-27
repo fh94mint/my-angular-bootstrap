e881e232c5ac29fa055d41ff3de8c0cb45054847
fh94mint <119185581+fh94mint@users.noreply.github.com>
2023-01-27 09:31:18 +0900

    Add PagesComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: AppComponent,
+    loadChildren: async (): Promise<typeof PagesModule> =>
+      import('./pages/pages.module').then((m) => m.PagesModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { BaseComponent } from '../base.component';
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+  styleUrls: ['./pages.component.scss'],
+})
+export class PagesComponent extends BaseComponent {}
