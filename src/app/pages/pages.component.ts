7659000129ca41eda2701f9bd342725578254ee5
fh94mint <119185581+fh94mint@users.noreply.github.com>
2023-01-27 14:26:26 +0900

    Fix PagesComponent.(Ngrx Component Store)

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { BaseComponent } from '../base.component';
+import { PagesStoreService } from './pages.store.service';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.scss'],
+  providers: [PagesStoreService],
 })
-export class PagesComponent extends BaseComponent {}
+export class PagesComponent extends BaseComponent {
+  constructor(private readonly pagesStoreService: PagesStoreService) {
+    super();
+  }
+}
--- src/app/pages/pages.store.service.ts
+++ src/app/pages/pages.store.service.ts
@@ -2,13 +2,24 @@ import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
 import { PagesStoreState } from './pages.store.interface';
 
-const pagesStoreInitialState: PagesStoreState = {};
+const pagesStoreInitialState: PagesStoreState = {
+  data: undefined,
+};
 
 @Injectable()
 export class PagesStoreService extends ComponentStore<PagesStoreState> {
   readonly initialize = this.updater((): PagesStoreState => {
     return pagesStoreInitialState;
   });
+
+  readonly update = this.updater<PagesStoreState>(
+    (state, value): PagesStoreState => {
+      return {
+        ...state,
+        ...value,
+      };
+    }
+  );
 
   constructor() {
     super(pagesStoreInitialState);
